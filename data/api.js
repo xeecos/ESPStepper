5a3b29dd9e1eb1ba4d5c673824d63c2adb7b0971
虎子哥 <flashindream@gmail.com>
2023-04-11 10:30:43 +0800

    update ui

--- data/api.js
+++ data/api.js
@@ -6,6 +6,27 @@ document.getElementById("speed-range").oninput = function () {
     document.getElementById("speed-input").value = Math.pow(2,document.getElementById("speed-range").value-1);
     localStorage.setItem("speed", this.value);
 }
+document.getElementById("current-input").value = document.getElementById("current-range").value = localStorage.getItem("current")||1000;
+document.getElementById("current-input").oninput = document.getElementById("current-range").oninput = function () {
+    document.getElementById("current-range").value = this.value;
+    document.getElementById("current-input").value = this.value;
+    localStorage.setItem("current", this.value);
+    onCurrent();
+}
+document.getElementById("ratio-input").value = document.getElementById("ratio-range").value = localStorage.getItem("ratio")||34;
+document.getElementById("ratio-input").oninput = document.getElementById("ratio-range").oninput = function () {
+    document.getElementById("ratio-range").value = this.value;
+    document.getElementById("ratio-input").value = this.value;
+    localStorage.setItem("ratio", this.value);
+    onRatio();
+}
+document.getElementById("microsteps-range").value = localStorage.getItem("microsteps")||9;
+document.getElementById("microsteps-input").value = Math.pow(2,document.getElementById("microsteps-range").value-1);
+document.getElementById("microsteps-range").oninput = function () {
+    document.getElementById("microsteps-input").value = Math.pow(2,document.getElementById("microsteps-range").value-1);
+    localStorage.setItem("microsteps", this.value);
+    onMicrosteps();
+}
 $(function () {
     window.Translator.translate(localStorage.getItem("lang"));
     document.getElementById("lang-select").value = localStorage.getItem("lang")||"english";
@@ -13,7 +34,6 @@ $(function () {
 document.getElementById("lang-select").oninput = function()
 {
     window.Translator.translate(this.value);
-    
     $(function () {
         window.Translator.translate(this.value);
     });
@@ -56,8 +76,32 @@ async function onBackwardDirection() {
 }
 function onSpeed(dir) {
     return new Promise(resolve => {
-        let speed = document.getElementById('speed-input').value;
+        let speed = document.getElementById('speed-input').value/240.0;
         request(`/speed/set?speed=${speed*dir}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function onMicrosteps() {
+    return new Promise(resolve => {
+        let microsteps = document.getElementById('microsteps-input').value;
+        request(`/stepper/set?microsteps=${microsteps}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function onCurrent() {
+    return new Promise(resolve => {
+        let current = document.getElementById('current-input').value;
+        request(`/stepper/set?current=${current}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function onRatio() {
+    return new Promise(resolve => {
+        let ratio = document.getElementById('ratio-input').value;
+        request(`/stepper/set?ratio=${ratio}`).then(res => {
             resolve(JSON.parse(res))
         });
     })
